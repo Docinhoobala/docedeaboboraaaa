0673537b36776447204979d85047e4a2848121e6
Docinhoobala <salesjoaquim364@gmail.com>
2025-03-12 16:38:10 -0300

    Add files via upload

---
+++ laços.py
@@ -0,0 +1,4 @@
+lista_numeros = [ 47, 49, 20, 396, 1520, 4]
+#estou criando uma lista de numeros aleatorios, quero percorer cada numero desta lista e imprimir em tela
+for n in lista_numeros:
+    print(f'O numero atual da lista de numeros e de {n}')
