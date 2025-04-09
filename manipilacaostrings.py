6030e5b83c5d95b83a382db238506992233033ff
Docinhoobala <salesjoaquim364@gmail.com>
2025-04-09 16:26:53 -0300

    Add files via upload

---
+++ manipilacaostrings.py
@@ -0,0 +1,6 @@
+nome='joaquim'
+
+print(nome[2])
+#todo string e indexavel ou seja consigo acessar letras de uma palavra
+#informando uma possiçao representada por []
+
---
+++ pi.py
@@ -0,0 +1,3 @@
+pi = 3.141111111
+print(f'o numero pi e de {pi:.2f}')#volta 2 casas decimais sem o :.f volta todas as casas decimais
+print(round(pi, 2))
