2c823e584c44d9668dcf4fdabd760bb5aebabc44
Docinhoobala <salesjoaquim364@gmail.com>
2025-04-09 16:34:22 -0300

    Add files via upload

--- manipilacaostrings.py
+++ manipilacaostrings.py
@@ -4,3 +4,5 @@ print(nome[2])
 #todo string e indexavel ou seja consigo acessar letras de uma palavra
 #informando uma possiçao representada por []
 
+megaseno = 43543545354355654565434543
+print(f'a premiaçao foi de {megaseno:,}')#separador de milhar
