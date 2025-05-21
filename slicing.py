276d0b0ff49414325ac9809294869221166f7361
Docinhoobala <salesjoaquim364@gmail.com>
2025-05-21 17:18:09 -0300

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,15 @@
+lista_nomes = [
+    'alvaro',
+    'leticia',
+    'joaquim',
+    'arthur',
+    'raul'
+]
+
+print(lista_nomes[0:4])
+
+print(lista_nomes[0:6])
+
+print(lista_nomes[1::2])
+
+print(lista_nomes[::-1])
